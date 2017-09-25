6f5a9a1b129bcd68691f975f2937bafc2bb166c5
JpnLavender <irimamekun@gmail.com>
2017-09-25 12:30:39 +0900

    Create a function to POST to slack and twitter

--- app.rb
+++ app.rb
@@ -5,7 +5,8 @@ require "sinatra/base"
 require "sinatra/reloader"
 require "pry" if development?
 require "json"
-
+require 'net/http'
+require 'uri'
 require "./models"
 
 not_found do
@@ -13,6 +14,7 @@ not_found do
 end
 
 get '/' do
+  sent_verification
   erb :post_form
 end
 
@@ -27,5 +29,68 @@ post '/register' do
 end
 
 post '/approval' do
-  p params
+  data = JSON.parse(params["payload"])
+  if data["actions"].first["name"] == "ok"
+    @rest = Twitter::REST::Client.new(
+      {
+        consumer_key: ENV.fetch("CONSUMER_KEY"),
+        consumer_secret: ENV.fetch("CONSUMER_SECRET"),
+        access_token: ENV.fetch("ACCESS_TOKEN"),
+        access_token_secret: ENV.fetch("ACCESS_TOKEN_SECRET")
+      }
+
+    )
+    tweet = Nana.find_by(id: data.value.to_i)
+    @rest.update("#{tweet.comment}\n#{tweet.file.medium.url}")
+    tweet.sent!
+  else
+    tweet.sent!
+  end
+  data["original_message"]["text"] = "Succes Tweet!"
+  json(data)
+end
+
+private
+
+def sent_verification 
+  Nana.where(status: "unsent").each do |nana|
+    uri = URI.parse("https://hooks.slack.com/services/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")
+    https = Net::HTTP.new(uri.host, uri.port)
+
+    https.use_ssl = true # HTTPSでよろしく
+    req = Net::HTTP::Post.new(uri.request_uri)
+
+    req["Content-Type"] = "application/json" # httpリクエストヘッダの追加
+    payload = {
+      "text": nana.comment,
+      "attachments": [
+        {
+          "fallback": "fallback string",
+          "callback_id": "callback_id value",
+          "color": "#FF0000",
+          "attachment_type": "default",
+          "image_url": nana.file.medium.url,
+          "actions": [
+            {
+              "name": "ok",
+              "text": "承認",
+              "type": "button",
+              "style":"default",
+              "value": nana.id
+            },
+            {
+              "name": "no",
+              "text": "拒否",
+              "type": "button",
+              "style":"danger",
+              "value": nana.id
+            }
+          ]
+        }
+      ]
+    }.to_json
+    req.body = payload
+    res = https.request(req)
+    # nana.verification! if development?
+  end
 end
