07f5dbe8e3e3190cdde631a67f6099ba6fbd88ff
JpnLavender <irimamekun@gmail.com>
2017-09-28 13:23:46 +0900

    Cleaning source code

--- app.rb
+++ app.rb
@@ -5,8 +5,6 @@ require "sinatra/base"
 require "sinatra/reloader"
 require "pry" if development?
 require "json"
-require 'net/http'
-require 'uri'
 require "./models"
 
 not_found do
@@ -14,12 +12,16 @@ not_found do
 end
 
 get '/' do
+  # session["responce"] = nil
   erb :post_form
 end
 
 get '/send_to_slack' do
-  sent_verification
-  json({status: 200} )
+  if Hoge.sent_verification == "OK"
+    json({status: 200})
+  else 
+    json({status: 500})
+  end
 end
 
 post '/register' do
@@ -39,86 +41,20 @@ end
 
 post '/approval' do
   data = JSON.parse(params["payload"])
-  tweet = Nana.find_by(id: data["actions"].first["value"].to_i)
+  nana = Nana.find_by(id: data["actions"].first["value"].to_i)
   if data["actions"].first["name"] == "ok"
 
-    if tweet
-      Hoge.twitter_auth.update (tweet.comment), { media_ids: tweet.media_ids }
-      tweet.sent!
+    if nana
+      Hoge.twitter_auth.update (nana.comment), { media_ids: nana.media_ids }
+      nana.sent!
       json({text: "ツイートしました！"})
 
     else
       json({text: "データが見つかりませんでした"})
     end
   else
-    tweet.sent!
+    nana.sent!
     json({text: "キャンセルしました"})
   end
 end
 
-private
-
-def sent_verification 
-  nanas = Nana.where(status: "unsent")
-  @payload = Hash.new
-  uri = URI.parse(ENV.fetch("SLACK_URL"))
-  https = Net::HTTP.new(uri.host, uri.port)
-
-  https.use_ssl = true # HTTPSでよろしく
-  req = Net::HTTP::Post.new(uri.request_uri)
-
-  req["Content-Type"] = "application/json" # httpリクエストヘッダの追加
-
-  if nanas.exists?
-    nanas.each do |nana|
-      color = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF"].sample
-      @payload = {
-        "text": "[参加団体の宣伝]\n\n" + nana.comment ,
-
-        "attachments": [
-          {
-            "color": color,
-            "attachment_type": "default",
-            "fields": [
-              {
-                "title": "現在の承認待ち数",
-                "value": Nana.where(status: "verification").count,
-                "short": true
-              }
-            ],
-          }
-        ]
-      }
-      nana.files.each_with_index do |v,i|
-        @payload[:attachments][i] ||= {}
-        @payload[:attachments][i].merge!({text: "#{i} image", image_url: v.medium.url, color: color})
-      end
-      @payload[:attachments].last.merge!({
-        "fallback": "fallback string",
-        "callback_id": "callback_id value",
-        "actions": [
-          {
-            "name": "ok",
-            "text": "承認",
-            "type": "button",
-            "style":"default",
-            "value": nana.id
-          },
-          {
-            "name": "no",
-            "text": "拒否",
-            "type": "button",
-            "style":"danger",
-            "value": nana.id
-          }
-        ]
-      })
-      nana.verification!
-    end
-  else
-    @payload = {"text": "承認待ちはありません"}
-  end
-  req.body = @payload.to_json
-  res = https.request(req)
-  pp @payload
-end
--- models.rb
+++ models.rb
@@ -3,6 +3,9 @@ Bundler.require
 require './carrier_wave'
 require 'carrierwave/orm/activerecord'
 require 'carrierwave/processing/rmagick'
+require 'net/http'
+require 'uri'
+require 'json'
 
 config  = YAML.load_file( './database.yml' )
 
@@ -49,6 +52,70 @@ class Hoge
         access_token_secret: ENV.fetch("ACCESS_TOKEN_SECRET")
       }
     )
+  end
+
+  def self.sent_verification
+    nanas = Nana.where(status: "unsent")
+    @payload = Hash.new
+    uri = URI.parse(ENV.fetch("SLACK_URL"))
+    https = Net::HTTP.new(uri.host, uri.port)
+
+    https.use_ssl = true # HTTPSでよろしく
+    req = Net::HTTP::Post.new(uri.request_uri)
+
+    req["Content-Type"] = "application/json" # httpリクエストヘッダの追加
+
+    if nanas.exists?
+      nanas.each do |nana|
+        color = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF"].sample
+        @payload = {
+          "text": "[参加団体の宣伝]\n\n" + nana.comment ,
+          "attachments": [
+            {
+              "color": color,
+              "attachment_type": "default",
+              "fields": [
+                {
+                  "title": "現在の承認待ち数",
+                  "value": Nana.where(status: "verification").count,
+                  "short": true
+                }
+              ],
+            }
+          ]
+        }
+        nana.files.each_with_index do |v,i|
+          @payload[:attachments][i] ||= {}
+          @payload[:attachments][i].merge!({text: "#{i} image", image_url: v.medium.url, color: color})
+        end
+        @payload[:attachments].last.merge!({
+          "fallback": "fallback string",
+          "callback_id": "callback_id value",
+          "actions": [
+            {
+              "name":  "ok",
+              "text":  "承認",
+              "type":  "button",
+              "style": "success",
+              "value":  nana.id
+            },
+            {
+              "name":  "no",
+              "text":  "拒否",
+              "type":  "button",
+              "style": "danger",
+              "value":  nana.id
+            }
+          ]
+        })
+        nana.verification!
+      end
+    else
+      @payload = {"text": "承認待ちはありません"}
+    end
+    req.body = @payload.to_json
+    res = https.request(req)
+    return res.msg
   end
 end
 
